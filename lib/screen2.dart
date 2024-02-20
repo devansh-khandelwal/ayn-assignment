93f7270c4912b05a318f18d6a70dcaf3ddb9da59
devansh-khandelwal <devanshk2107@gmail.com>
2024-02-20 16:10:33 +0530

    added screen 2

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,15 +1,75 @@
 // screen2.dart
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget {
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  List<dynamic> _posts = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPosts();
+  }
+
+  Future<void> _fetchPosts() async {
+    try {
+      final response = await Dio().get('http://jsonplaceholder.typicode.com/posts');
+      setState(() {
+        _posts = response.data;
+        _isLoading = false;
+      });
+    } catch (e) {
+      print(e.toString());
+      // Handle error gracefully
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Screen 2'),
+        title: Text('API Data Fetching'),
       ),
-      body: Center(
-        child: Text('This is Screen 2'),
+      body: _isLoading
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : ListView.separated(
+        itemCount: _posts.length,
+        itemBuilder: (context, index) {
+          final post = _posts[index];
+          return Container(
+            margin: EdgeInsets.symmetric(vertical: 6.0, horizontal:  8.0), // Add margin to each tile on all sides
+            decoration: BoxDecoration(
+              color: Color(0xFFE3F2FD), // Set background color of tile
+              borderRadius: BorderRadius.circular(8.0), // Optional: Add border radius for rounded corners
+            ),
+            child: ListTile(
+              title: Text(
+                post['title'],
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, // Make title bolder
+                  fontSize: 18.0, // Make title slightly bigger
+                ),
+              ),
+              subtitle: Text(
+                post['body'],
+                style: TextStyle(
+                  color: Colors.black, // Set body text color to black
+                ),
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
       ),
     );
   }
